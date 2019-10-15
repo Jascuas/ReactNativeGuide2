fce4a28162f25fb349e6ea9c7761121ce7022184
Jascuas <javiersanchezdelago@hotmail.com>
2019-10-15 11:25:21 +0200

    Input component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,30 @@
 import React from "react";
-import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import Card from "../components/Card";
 import Colors from "../constants/colors";
-
+import Input from "../components/Input";
 const StartGameScreen = props => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a new game!</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a number: </Text>
-        <TextInput />
+        <Input
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="number"
+          maxLength={2}
+          
+        />
         <View style={styles.buttonContainer}>
-          <View style={styles.button}><Button color={Colors.accent} title="Reset" onPress={() => {}} /></View>
-          <View style={styles.button}><Button color={Colors.primary}  title="Confirm" onPress={() => {}} /></View>
+          <View style={styles.button}>
+            <Button color={Colors.accent} title="Reset" onPress={() => {}} />
+          </View>
+          <View style={styles.button}>
+            <Button color={Colors.primary} title="Confirm" onPress={() => {}} />
+          </View>
         </View>
       </Card>
     </View>
@@ -32,7 +44,7 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: 300,
     maxWidth: "80%",
-    alignItems: "center",
+    alignItems: "center"
   },
   buttonContainer: {
     flexDirection: "row",
@@ -40,8 +52,12 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     paddingHorizontal: 15
   },
-  button:{
+  button: {
     width: 100
+  },
+  input: {
+    width: 50,
+    textAlign: "center"
   }
 });
 
